d40916a811be4a41b98f817c98eb6961ebce0319
Gabriel Leme dos Santos <gabriellemesantos@gmail.com>
2023-01-30 21:53:57 -0300

    28º dia de CTD - edits no css, js e readme

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,5 +1,31 @@
 $('.row__posters, .row__poster, .row__posterLarge').slick({
   infinite: true,
+  speed: 300,
   slidesToShow: 8,
-  slidesToScroll: 2
+  slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
